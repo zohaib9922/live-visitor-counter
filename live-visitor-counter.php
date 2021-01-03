d3c879e671faa084812b4f9ae5088f85267418f9
Zohaib Hassan <zohaibhassan@Zohaibs-MacBook-Pro.local>
2021-01-03 11:00:00 +0500

    Initial plugin structure

---
+++ live-visitor-counter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Live Visitor Counter
+ * Description: Shows live visitor count on posts/pages.
+ * Author: Zohaib Hassan
+ * Version: 1.0.0
+ */
+
+if (!defined('ABSPATH')) exit;
+
+function lvc_enqueue_assets() {
+    wp_enqueue_script(
+        'lvc-js',
+        plugin_dir_url(__FILE__) . 'assets/js/lvc.js',
+        array(),
+        '1.0.0',
+        true
+    );
+}
+add_action('wp_enqueue_scripts', 'lvc_enqueue_assets');
